efac8a15cefd735b16f9cc57f610d61538bf6514
Johannes Danielsson <danielssonjohannes@gmail.com>
2022-08-23 17:19:10 +0200

    Menu for Lab part 1
    Made a menu so you will be able to pick between 5 different  choices

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,58 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void alternativeMenu(){
+        System.out.println("1. Upp and down ");
+        System.out.println("2. Min and Max ");
+        System.out.println("3. Rock, paper, scissor ");
+        System.out.println("4. In Order ");
+        System.out.println("e. Exit ");
+        Scanner alternativeIn= new Scanner(System.in);
+        String alternative = alternativeIn.next();
+        if (alternative.equals("1")){
+            uppAndDown();
+        }
+        if (alternative.equals("2")){
+            minAndMax();
+        }
+        if (alternative.equals("3")){
+            rockPaperScissor();
+        }
+        if (alternative.equals("4")){
+            inOrder();
+
+        }
+        if (alternative.equals("e") || alternative.equals("E")){
+            exit();
+        }
+
+
+    }
+
+    private static void exit() {
+    }
+
+    private static void inOrder() {
+        System.out.println("4");
+    }
+
+    private static void rockPaperScissor() {
+        System.out.println("3");
+    }
+
+    private static void minAndMax() {
+        System.out.println("2");
+    }
+
+    private static void uppAndDown() {
+        System.out.println("1");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        alternativeMenu();
+
     }
+
 }
